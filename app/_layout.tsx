94bdc669033ac2bef78d1a37855f8b2efe6cf450
Bereket-Ketema <bereketketema866@gmail.com>
2026-04-02 06:27:45 +0300

    the context provider have been wrapped to the stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,18 @@
 import { useColorScheme } from "@/app-example/hooks/use-color-scheme";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useContext } from "react";
+import { TaskProvider } from "@/context/Taskcontext";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   return(
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <TaskProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
+      </TaskProvider>
     </ThemeProvider>
   );
 }
